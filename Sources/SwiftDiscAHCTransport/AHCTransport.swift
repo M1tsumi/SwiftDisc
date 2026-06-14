09bfaf216062892906855d10b48e684091ecf20f
M1tsumi <0000imdumb0000@gmail.com>
2026-06-14 16:07:40 -0400

    Test minimal AHCTransport stub to isolate failure cause

--- Sources/SwiftDiscAHCTransport/AHCTransport.swift
+++ Sources/SwiftDiscAHCTransport/AHCTransport.swift
@@ -18,27 +18,6 @@ public final class AHCTransport: HTTPTransport, @unchecked Sendable {
     }
 
     public func request(method: String, url: URL, body: Data?, headers: [String: String]?) async throws -> HTTPResponse {
-        var req = HTTPClientRequest(url: url.absoluteString)
-        req.method = .init(rawValue: method)
-        if let body {
-            req.body = .bytes(body)
-        }
-        if let headers {
-            for (key, value) in headers {
-                req.headers.add(name: key, value: value)
-            }
-        }
-        let response = try await client.execute(req)
-        let data: Data
-        if let bodyBytes = response.body {
-            data = bodyBytes.withUnsafeReadableBytes { Data($0) }
-        } else {
-            data = Data()
-        }
-        var respHeaders = [String: String]()
-        for (key, value) in response.headers {
-            respHeaders[key] = value
-        }
-        return HTTPResponse(data: data, statusCode: Int(response.status.code), headers: respHeaders)
+        throw DiscordError.network(NSError(domain: "NotImplemented", code: -1, userInfo: nil))
     }
 }
