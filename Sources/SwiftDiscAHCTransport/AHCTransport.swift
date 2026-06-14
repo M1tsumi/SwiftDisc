173b6382692198e5925561cfc747501b883553ce
M1tsumi <0000imdumb0000@gmail.com>
2026-06-14 16:13:04 -0400

    Test bare-minimum AHC stub (no HTTPClient, no init body)

--- Sources/SwiftDiscAHCTransport/AHCTransport.swift
+++ Sources/SwiftDiscAHCTransport/AHCTransport.swift
@@ -3,18 +3,7 @@ import AsyncHTTPClient
 import SwiftDisc
 
 public final class AHCTransport: HTTPTransport, @unchecked Sendable {
-    private let client: HTTPClient
-
     public init(proxy: ProxyConfiguration? = nil) {
-        var config = HTTPClient.Configuration()
-        if let proxy {
-            config.proxy = .server(host: proxy.host, port: proxy.port)
-        }
-        self.client = HTTPClient(eventLoopGroupProvider: .createNew, configuration: config)
-    }
-
-    deinit {
-        try? client.syncShutdown()
     }
 
     public func request(method: String, url: URL, body: Data?, headers: [String: String]?) async throws -> HTTPResponse {
