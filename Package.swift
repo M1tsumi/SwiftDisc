3922f183d804e6daf73f61c5a15750852f12bdff
M1tsumi <0000imdumb0000@gmail.com>
2026-06-14 16:37:57 -0400

    Try async-http-client 1.25+ for Swift 6.2 compat

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
         .library(name: "SwiftDiscAHCTransport", targets: ["SwiftDiscAHCTransport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", exact: "1.19.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.25.0"),
     ],
     targets: [
         .target(
