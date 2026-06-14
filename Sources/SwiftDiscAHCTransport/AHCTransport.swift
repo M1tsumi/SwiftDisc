f1620dc0d8706d51d58b215c67729364cd38ee0a
M1tsumi <0000imdumb0000@gmail.com>
2026-06-14 16:24:43 -0400

    Test minimal AHC with Foundation import and @unchecked Sendable

--- Sources/SwiftDiscAHCTransport/AHCTransport.swift
+++ Sources/SwiftDiscAHCTransport/AHCTransport.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftDisc
 
 public final class AHCTransport: HTTPTransport, @unchecked Sendable {
-    public init(proxy: ProxyConfiguration? = nil) {
+    public init() {
     }
 
     public func request(method: String, url: URL, body: Data?, headers: [String: String]?) async throws -> HTTPResponse {
