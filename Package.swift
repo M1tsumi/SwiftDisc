8e7e8950895b4377e9f80bfd5bff4213ed61d946
M1tsumi <0000imdumb0000@gmail.com>
2026-06-14 15:55:09 -0400

    Add swift-docc-plugin dependency for documentation generation

--- Package.swift
+++ Package.swift
@@ -45,10 +45,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "SwiftDisc",
+            plugins: [.plugin(name: "Swift-DocC", package: "swift-docc-plugin")],
             swiftSettings: [.swiftLanguageMode(.v5)]
         ),
         .target(
